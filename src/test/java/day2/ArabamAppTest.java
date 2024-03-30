27a8661281b47f1eda5d98649c07d3a4d05d0266
Sevket Bugra Yilmaz <sbugra4@gmail.com>
2024-03-30 19:19:57 +0300

    day2 commit

--- src/test/java/day2/ArabamAppTest.java
+++ src/test/java/day2/ArabamAppTest.java
@@ -1,8 +1,11 @@
 package day2;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
@@ -10,6 +13,7 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class ArabamAppTest {
@@ -36,7 +40,7 @@ public void setUp() throws MalformedURLException {
 
 
 @Test
-    public void arabamTest() {
+    public void arabamTest() throws InterruptedException {
 
     /*
     driver.activateApp("com.dogan.arabam");
@@ -47,18 +51,50 @@ public void setUp() throws MalformedURLException {
     // 1. yol olarak uygulamanin kimlik bilgisi uzerinden uygulama driver methoduyla aktif hale getirilebilir.
     // uygulaminin basarili bir sekilde acildigi dogrulanir
     Assert.assertTrue(driver.isAppInstalled("com.dogan.arabam"));
-    // alt menuden ilan ara butonuna tiklanir
+    // uygulaminin basarili bir sekilde acildigi dogrulanir
     Assert.assertTrue(driver.findElementByAccessibilityId("İlan Ver").isDisplayed());
+    // alt menuden ilan ara butonuna tiklanir
+    driver.findElementByXPath("//*[@text='İlan Ara']").click();
     // kategori olarak otomobil secilir
+    driver.findElementByXPath("//*[@text='Otomobil']").click();
+    Thread.sleep(2000);
     // arac olarak Volkswagen secilir
+    TouchAction action = new TouchAction<>(driver);
+    action
+            .press(PointOption.point(530,1553)) // ekranda kaydirma islemini baslatmak icin belirledigimiz ilk baslangic noktasi
+            .waitAction(WaitOptions.waitOptions(Duration.ofMillis(100)))
+            // kaydirma islemi gerceklesirken baslangic noktasi ile (press) ile son (moveTo) arasinda gecen zaman
+            // Eger bu sure arttilirsa gidilen yol miktari kisalir yani daha az yol kat ederiz
+            // Eger bu sure azaltilirsa gidilen yol mesafe miktari artar daha fazla yol kat etmis oluruz.
+            .moveTo(PointOption.point(530,336)) // ekranda kaydirma islemini bitirmek icin kaydirdigimiz son nokta
+            .release() // parmagimizi ekrandan kaldirma ve kaydirma islemini sonlandirma
+            .perform(); // verilen action gorevlerini yerine getirme
+
+       // driver.findElementByXPath("//*[@text='Volkswagen']").click();
+        // 230 1289
+        action.
+                press(PointOption.point(230,1289))
+                .release()
+                .perform();
+
     // arac markasi olarak passat secilir
+    Thread.sleep(2000);
+    driver.findElementByXPath("//*[@text='Passat']").click();
     // 1.4 TSI BlueMotion secilir
+    driver.findElementByXPath("//*[@text='1.4 TSi BlueMotion']").click();
     // Paket secimi yapilir
+    driver.findElementByXPath("//*[@text='Highline']").click();
     // Ucuzdan pahaliya siralama yaparak filtreleme yapilir
+    driver.findElementById("com.dogan.arabam:id/textViewSorting").click();
+    driver.findElementByXPath("//*[@text='Fiyat - Ucuzdan Pahalıya']").click();
     // Gelen en ucuz aracin 500.000 tl den buyuk oldugu dogrulanir
+    String enUcuzAracFiyati = driver.findElementByXPath("(//*[@resource-id='com.dogan.arabam:id/tvPrice'])[1]").getText();
+    System.out.println(enUcuzAracFiyati); // 760.000 TL
 
+    enUcuzAracFiyati=enUcuzAracFiyati.replaceAll("\\D","");
+    System.out.println(enUcuzAracFiyati); // 760000
 
-
+    Assert.assertTrue(Integer.parseInt(enUcuzAracFiyati)>500000);
 
 
 
